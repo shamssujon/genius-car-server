5b1a3b3b00f1ee59adc95dac64f8682558a6a24c
Shams Sujon <sujon2762@gmail.com>
2022-11-06 04:50:59 +0600

    Setup MongoDB with ENV

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 7100;
+
+require("dotenv").config();
 
 // Middlewares
 app.use(cors());
@@ -13,4 +17,23 @@ app.listen(port, (req, res) => {
 
 app.get("/", (req, res) => {
     res.send(`Genius car server is running on port: ${port}`);
+});
+
+
+// Get username password from env file
+const userName = process.env.DB_USER_NAME;
+const password = process.env.DB_PASSWORD;
+
+// MongoDB
+const uri = `mongodb+srv://${userName}:${password}@cluster0.9q7qmdx.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
 });
